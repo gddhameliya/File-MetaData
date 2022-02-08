b885d0c8857a678227c2c1eaeb30b89bde78982b
gddhameliya <gaurangddhameliya123@gmail.com>
2022-02-08 14:33:35 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,28 @@
+var express = require("express");
+const multer = require("multer");
+var cors = require("cors");
+require("dotenv").config();
+
+var app = express();
+const upload = multer({ dest: "uploads/" });
+
+app.use(cors());
+app.use("/public", express.static(process.cwd() + "/public"));
+
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
+});
+
+app.post("/api/fileanalyse", upload.single("upfile"), (req, res) => {
+  const result = {
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size,
+  };
+  res.send(result);
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log("Your app is listening on port " + port);
+});
